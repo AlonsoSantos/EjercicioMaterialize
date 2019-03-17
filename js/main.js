7c9ed9b12f7070d08ec92bd2e8e24bcd4633b61b
AlonsoSantos <ojimenez34@misena.edu.co>
2019-03-17 18:19:32 -0500

    terminado

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// Initialize collapse button
+$(".button-collapse").sideNav();
+$(".drepdown-button").dropdown();
+// Initialize collapsible (uncomment the line below if you use the dropdown variation)
+//$('.collapsible').collapsible();
+$(document).ready(function(){
+    $('.carousel').carousel();
+});
